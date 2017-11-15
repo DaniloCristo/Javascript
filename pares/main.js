b311092292358f61f54b8f180a703002350ed537
DaniloCristo <danilomarques20@hotmail.com>
2017-11-15 00:03:10 -0200

    Arrow Function

--- pares/main.js
+++ pares/main.js
@@ -1,9 +1,8 @@
 function pares(array){
-	
+
 	//função callback que filtra os valores retornando a array apenas com valores pares
-	let pares = array.filter(function(val){
-		return val % 2 == 0;
-	});
+	let pares = array.filter(val => val % 2 == 0);
+	
 	return pares;
 }
 console.log(pares([4,8,7,9,2,22]));
