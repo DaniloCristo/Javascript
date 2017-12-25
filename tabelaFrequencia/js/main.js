35509212c6fc2346cec6820a615eed8d65305a41
Danilo Marques <danilomarques20@hotmail.com>
2017-12-25 13:23:36 -0300

    Criação de uma tabela de frequencia

---
+++ tabelaFrequencia/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	
+	
+	function tabelaFrequencia(array) {
+		//criando a tabela
+		let tabela = "<table class='table table-striped'>";
+			
+			//criando as colunas da tabela
+			tabela += "<thead><tr><th>idade</th><th>Frequência</th></tr></thead>";
+			let newArray = []; 
+			
+			let freq = [];
+			//console.log(array)
+			
+			//povoando uma nova array com os números sem suas repetições, que ficaram na coluna "idade"
+			for(let i = 0; i<array.length; i++) {
+
+				if(newArray.indexOf(array[i]) == -1) {
+					newArray.push(array[i]);
+				}
+				
+			}
+
+			for(let i = 0; i<newArray.length; i++) {
+				let temp = 0;
+				for(let j = 0; j<array.length; j++){
+					if(newArray[i] == array[j]){
+						temp++;
+					}
+				}
+				freq.push(temp);
+			}
+			tabela += "<tbody>";
+			
+			for(let i = 0; i<newArray.length;i++) {
+				//colocando as idades
+				tabela += "<tr><td>"+newArray[i]+"</td><td>"+freq[i]+"</td></tr>";
+			}
+			
+
+			
+		
+			tabela += "</tbody>";
+			tabela +="</table>";
+
+			$("#tabela").html(tabela);
+
+			console.log(newArray)
+			console.log(freq)
+
+
+
+	}
+
+	tabelaFrequencia([5,7,5,9,7,7,6,9,9,9,10,12,12,7]);
+})
